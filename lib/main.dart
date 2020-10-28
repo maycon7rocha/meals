cc3b21ed423d1c2a1689008305ff84baccb1e7b1
Maycon Rocha <maycon7rocha@gmail.com>
2020-10-28 20:39:03 -0300

    aula 182 - ingredientes

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,9 @@ import 'package:meals/screens/categories_meals_screen.dart';
 import 'package:meals/screens/meal_detail_screen.dart';
 import 'screens/categories_screens.dart';
 import './utils/app_routes.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -17,17 +17,34 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Raleway',
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
         textTheme: ThemeData.light().textTheme.copyWith(
-          title: TextStyle(
-            fontSize: 20,
-            fontFamily: 'RobotoCondensed',
-          ),
-        ),
-
+              title: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
       ),
       routes: {
         AppRoutes.HOME: (ctx) => CategoriesScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
         AppRoutes.MEAL_DETAIL: (ctx) => MealDeatailScreen(),
+      },
+      // onGenerateRoute: (settings) {
+      //   if (settings.name == '/alguma-coisa') {
+      //     return null;
+      //   } else {
+      //     return MaterialPageRoute(
+      //       builder: (_) {
+      //         return CategoriesScreen();
+      //       },
+      //     );
+      //   }
+      // },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (_) {
+            return CategoriesScreen();
+          },
+        );
       },
     );
   }
