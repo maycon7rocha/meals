772fb107538fcf23a284e9dcd140d8b2527ee5eb
Maycon Rocha <maycon7rocha@gmail.com>
2020-10-28 11:34:59 -0300

    aula 173 - passando informação para outra tela

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
-  const CategoriesMealsScreen({Key key}) : super(key: key);
+  final Category category;
+  const CategoriesMealsScreen(this.category);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Receitas'),
+        title: Text(category.title),
       ),
       body: Center(
-        child: Text('Receitas por Categoria'),
+        child: Text(category.title),
       ),
     );
   }
