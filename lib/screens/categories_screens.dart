730d92bf0d3a7abb20bee2293be871e17e629463
Maycon Rocha <maycon7rocha@gmail.com>
2020-10-28 11:29:19 -0300

    aula 172 navegação

--- lib/screens/categories_screens.dart
+++ lib/screens/categories_screens.dart
@@ -11,8 +11,8 @@ class CategoriesScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Vamos Cozinhar?'),
       ),
-          body: GridView(
-            padding: const EdgeInsets.all(25),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 3 / 2,
