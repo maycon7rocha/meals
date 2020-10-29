4f9f9edd783921322cbddf51dfcd1e14569113c8
Maycon Rocha <maycon7rocha@gmail.com>
2020-10-29 10:40:39 -0300

    aula 184 - navegação por tab em cima

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:meals/screens/categories_meals_screen.dart';
 import 'package:meals/screens/meal_detail_screen.dart';
 import 'screens/categories_screens.dart';
 import './utils/app_routes.dart';
-
+import 'screens/tabs_screen.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
             ),
       ),
       routes: {
-        AppRoutes.HOME: (ctx) => CategoriesScreen(),
+        AppRoutes.HOME: (ctx) => TabsScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
         AppRoutes.MEAL_DETAIL: (ctx) => MealDeatailScreen(),
       },
---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import 'categories_screens.dart';
+import 'favorite_screen.dart';
+
+class TabsScreen extends StatelessWidget {
+  const TabsScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Vamos Cozinhar?'),
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.category),
+                text: 'Categorias',
+              ),
+              Tab(
+                icon: Icon(Icons.favorite),
+                text: 'Favoritos',
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            CategoriesScreen(),
+            FavoriteScreen(),
+          ],
+        ),
+      ),
+    );
+  }
+}
